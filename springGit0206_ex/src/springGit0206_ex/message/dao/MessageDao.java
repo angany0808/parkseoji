339e91d380e18b11a08fb6c092c12e3a84b55972
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-06 13:20:27 +0900

    셀렉트ㅇ

--- springGit0206_ex/src/springGit0206_ex/message/dao/MessageDao.java
+++ springGit0206_ex/src/springGit0206_ex/message/dao/MessageDao.java
@@ -1,5 +1,14 @@
 package springGit0206_ex.message.dao;
 
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import springGit0206_ex.message.dto.MessageDto;
+import springGit0206_ex.message.mapper.SelectMapper;
+
 public class MessageDao {
+
+
 
 }
