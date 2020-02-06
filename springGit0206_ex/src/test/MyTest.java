0532a2c3e853ce5b6ecb7f0036ba213be698b7ba
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-06 13:05:02 +0900

    인서트 업데이트 푸시 태환

--- springGit0206_ex/src/test/MyTest.java
+++ springGit0206_ex/src/test/MyTest.java
@@ -2,13 +2,35 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import springGit0206_ex.message.dto.MessageDto;
+import springGit0206_ex.message.mapper.InsertMapper;
+import springGit0206_ex.message.mapper.UpdateMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:config/springContext.xml")
 public class MyTest {
-
+	
+	@Autowired
+	InsertMapper insertMapper;
+	@Autowired
+	UpdateMapper updateMapper;
+	
+	@Test@Ignore
+	public void insertTest() {
+		insertMapper.insert(new MessageDto(0, "지태환", "퍼킹", LocalDateTime.now()));
+	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void updateTest() {
+		updateMapper.update(new MessageDto(17, "하하", "로사 오우 내사랑 로싸", null));;
 	}
 
 }
